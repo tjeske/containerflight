2df79a6e60825446fc693becb333d8ed27bae2e6
Tobias Jeske <14035220+tjeske@users.noreply.github.com>
2018-04-22 15:03:35 +0200

    update examples

--- do_test.sh
+++ do_test.sh
@@ -5,5 +5,7 @@ go install
 
 ../../../../bin/containerflight run ./examples/ansible ansible-playbook --version
 ../../../../bin/containerflight run ./examples/python3.7 --version
+../../../../bin/containerflight run ./examples/youtube-dl --version
 ../../../../bin/containerflight run ./examples/gitg
+../../../../bin/containerflight run ./examples/hugin
 ../../../../bin/containerflight run ./examples/eclipse
